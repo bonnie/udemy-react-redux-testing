f9c35cacf7ee1a1abab1869ebe616f3de9fde357
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-01-05 22:05:07 -0800

    Add test for clearing textbox on submit

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -8,15 +8,19 @@ export default class CommentBox extends Component {
   handleChange(event) {
     this.setState({ comment: event.target.value })
   }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({ comment: '' })
+  }
   render() {
     return (
-    <div className="comment-box">
+    <form onSubmit={this.handleSubmit.bind(this)}className="comment-box">
       <textarea 
         onChange={this.handleChange.bind(this)}
         value={this.state.comment}
       />
       <button>Submit Comment</button>
-    </div>
+    </form>
     )
   }
 }
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -32,7 +32,8 @@ describe('CommentBox', () => {
     })
   
     it('when submitted, clears the input', () => {
-      
+      component.simulate('submit')
+      expect(textarea).to.have.value('')
     });
   });
 })
